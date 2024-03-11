977754977474dd125165563c0ad0fdbbeadcf6c1
Keshav Singh <keshavsingh4522@gmail.com>
2024-03-11 14:44:04 +0530

    OMKR-1 | A demo file added for testing runbook

---
+++ src/Azure/Scripts/demo-file.ps1
@@ -0,0 +1,9 @@
+# function Print-Text {
+#     Param (
+#         [string]$Text = "Hello, World!"
+#     )
+#     Write-Output $Text
+# }
+# Export-ModuleMember -Function Print-Text
+
+echo "hello world"
