6943274ad327f4644f32ff0c49de24caf0a421e9
Keshav Singh <keshavsingh4522@gmail.com>
2024-12-25 18:53:00 +0530

    OMKR-202 | Removed unused code

--- ui/common/footer/footer.js
+++
@@ -1,6 +0,0 @@
-// Load footer HTML
-fetch('ui/common/footer/footer.html')
-.then(response => response.text())
-.then(data => {
-    document.getElementById('footer-container').innerHTML = data;
-});
